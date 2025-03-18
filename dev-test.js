1566a45f8bfe45c5b991069a6def87c7bc40e436
waseem <msteev7@gmail.com>
2025-03-18 09:37:39 +0000

    Main

---
+++ dev-test.js
@@ -0,0 +1,9 @@
+// call: npm run dev-test
+
+/* Import */
+const Block = require("./block");
+
+/*  Test der Block-Funktionalität  */
+
+const block = new Block("aktuelle Zeit", "hash0", "hash1", "meine Daten");
+console.log(block. toString());
